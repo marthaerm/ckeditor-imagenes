09d5bb96cc49fb37c2dc0ce71c8887c57a21d435
MarthaRM <rdz.marthaelena@gmail.com>
2020-05-11 13:06:34 -0500

    Correcion de url de bootstrap

--- resources/views/ajax-sample.blade.php
+++ resources/views/ajax-sample.blade.php
@@ -8,7 +8,7 @@
     <!-- Importar JQuery-->
     <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 
-    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('../css/bootstrap.min.css')}}">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
